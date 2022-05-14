bc30aca48c99616f69bbdca156e92019f341d2ca
JIWEON-JEONG <dlektl6@naver.com>
2022-05-14 15:43:39 +0900

    [ADD] java study algo tostring

--- src/com/company/javacafe1week/Test.java
+++ src/com/company/javacafe1week/Test.java
@@ -1,12 +1,12 @@
 package com.company.javacafe1week;
 
 public class Test {
-    public static void main(String[] args) {
-        Test T = new Test();
-        System.out.println("Hello Java!");
-
-        System.out.println(T.hello());
-    }
+//    public static void main(String[] args) {
+//        Test T = new Test();
+//        System.out.println("Hello Java!");
+//
+//        System.out.println(T.hello());
+//    }
 
     public String hello(){
         return "Hello Method!";
